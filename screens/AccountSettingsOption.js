3d8c562e0dfb87f70e8997b22d72ecb2618185bf
Aaron Jan Inlayo <qajinlayo1@tip.edu.ph>
2024-12-07 16:24:58 +0800

    Account Editing Update

--- screens/AccountSettingsOption.js
+++ screens/AccountSettingsOption.js
@@ -1,228 +1,159 @@
 import * as React from "react";
 import { Image } from "expo-image";
-import { StyleSheet, Text, View, Pressable } from "react-native";
+import { StyleSheet, Text, View, Pressable, FontSize } from "react-native";
 import { useNavigation } from "@react-navigation/native";
-import { Border, Color, FontSize, FontFamily } from "../GlobalStyles";
+import { Border, Color, FontFamily } from "../GlobalStyles";
 import Icon from "react-native-vector-icons/Ionicons";
 
 const AccountSettingsOption = () => {
   const navigation = useNavigation();
 
   return (
-    <View style={styles.accountSettingsOption2}>
+    <View style={styles.Container1}>
       <Pressable
-        style={[styles.arrowleft]}
+        style={[styles.BackButton]}
         onPress={() => navigation.navigate("HomeScreen")}
       >
         <Icon name="arrow-back" size={25} color="#132A17" />
       </Pressable>
 
-      <Text style={styles.username}>USERNAME</Text>
-      <View
-        style={[styles.accountSettingsOption2Child, styles.accountLayout1]}
-      />
-      <View
-        style={[styles.accountSettingsOption2Item, styles.accountLayout1]}
-      />
-      <Text style={[styles.userInfo, styles.accountTypo]}>USER INFO</Text>
-      <Text style={[styles.account, styles.accountTypo]}>ACCOUNT</Text>
-      <Icon
-        name="person-circle"
-        size={95}
-        color="#132A17"
-        style={[styles.profile]}
-      />
-      <Text style={[styles.username1, styles.emailTypo]}>USERNAME</Text>
-      <Icon
-        name="mail"
-        size={35}
-        color="#132A17"
-        style={[styles.usernameIcon]}
-      />
-      <Text style={[styles.password, styles.emailTypo]}>PASSWORD</Text>
-      <Icon
-        name="lock-closed"
-        size={35}
-        color="#132A17"
-        style={[styles.passwordIcon]}
-      />
-      <Pressable
-        style={styles.rectanglePressable}
-        onPress={() => navigation.navigate("UserInfo")}
-      >
-        <Text style={[styles.editUserInfo, styles.emailTypo]}>
-          EDIT USER INFO
-        </Text>
-      </Pressable>
+      <View style={styles.Container2}>
+        <View style={styles.Icon}>
+          <Icon name="person-circle-outline" size={125} color="#132A17" />
+        </View>
+
+        <View style={styles.Child}>
+          <View style={styles.IconChild}>
+            <Icon name="person-outline" size={40} color="#132A17" />
+          </View>
+          <Text style={styles.ChildText}>USERNAME</Text>
+        </View>
+
+        <View style={styles.Child}>
+          <View style={styles.IconChild}>
+            <Icon name="mail-outline" size={40} color="#132A17" />
+          </View>
+          <Text style={styles.ChildText}>EMAIL</Text>
+        </View>
+
+        <View style={styles.Child}>
+          <View style={styles.IconChild}>
+            <Icon name="lock-closed-outline" size={40} color="#132A17" />
+          </View>
+          <Text style={styles.ChildText}>PASSWORD</Text>
+        </View>
+      </View>
+      <View style={styles.Container3}>
+        <Pressable
+          style={({ pressed }) => [
+            styles.EditButton,
+            pressed && styles.pressed,
+          ]}
+          onPress={() => navigation.navigate("UserInfo")}
+        >
+          <Text style={styles.EditButtonText}>Edit User Info</Text>
+        </Pressable>
+
+        <Pressable
+          style={({ pressed }) => [
+            styles.EditButton2,
+            pressed && styles.pressed,
+          ]}
+          onPress={() => navigation.navigate("UserInfo")}
+        >
+          <Text style={styles.EditButtonText}>Log Out</Text>
+        </Pressable>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  accountLayout1: {
-    borderRadius: Border.br_xl,
-    position: "absolute",
-  },
-  accountTypo: {
-    color: "white",
-    fontSize: 18,
-    top: "39.84%",
-    width: "31.39%",
-    height: "3.59%",
-    textAlign: "center",
-    fontFamily: FontFamily.poppinsExtraBold,
-    fontWeight: "800",
-    position: "absolute",
-  },
-  emailTypo: {
-    fontFamily: "Poppins-SemiBold",
-    fontWeight: "600",
-    textAlign: "center",
-    position: "absolute",
-    fontSize: 22,
-  },
-  accountLayout: {
-    left: "26.67%",
-    right: "10.56%",
-    width: "62.78%",
-    height: "0.16%",
-    maxHeight: "100%",
-    maxWidth: "100%",
-    position: "absolute",
-    overflow: "hidden",
-  },
-  arrowleft: {
-    height: "15.47%",
-    width: "25.53%",
-    top: "7.28%",
-    right: "86.81%",
-    bottom: "93.25%",
-    left: "3.67%",
-    position: "absolute",
-    overflow: "hidden",
-  },
-  username: {
-    height: "4.53%",
-    width: "45.83%",
-    top: "32.97%",
-    left: "27.11%",
-    fontSize: 24,
-    textAlign: "center",
-    color: Color.colorBlack,
-    fontFamily: FontFamily.poppinsExtraBold,
-    fontWeight: "800",
-    position: "absolute",
-  },
-  accountSettingsOption2Child: {
-    height: "6.25%",
-    width: "88.33%",
-    top: "38.59%",
-    right: "5.83%",
-    bottom: "55.16%",
-    left: "5.83%",
-    shadowColor: "rgba(0, 0, 0, 0.25)",
-    shadowOffset: {
-      width: 2,
-      height: 6,
-    },
-    shadowRadius: 4,
-    elevation: 4,
-    shadowOpacity: 1,
-    backgroundColor: Color.colorSeagreen,
-  },
-  accountSettingsOption2Item: {
-    height: "5%",
-    width: "41.94%",
-    top: "39.22%",
-    right: "49.44%",
-    bottom: "55.78%",
-    left: "8.61%",
-    backgroundColor: "rgba(246, 212, 186, 0.72)",
-  },
-  userInfo: {
-    left: "13.89%",
-    color: "white",
-  },
-  account: {
-    left: "56.39%",
-  },
-  profile: {
-    height: "13.91%",
-    width: "31.89%",
-    top: "18.44%",
-    right: "38.06%",
-    bottom: "68.75%",
-    left: "35.06%",
-  },
-  username1: {
-    top: "50.80%",
-    fontSize: FontSize.size_sm,
-    left: "25.83%",
-    fontWeight: "600",
-    color: Color.colorBlack,
-  },
-  email: {
-    top: "59.93%",
-    fontSize: FontSize.size_sm,
-    left: "25.83%",
-    fontWeight: "600",
-    color: Color.colorBlack,
-  },
-  password: {
-    top: "69.44%",
-    fontSize: FontSize.size_sm,
-    left: "25.83%",
-    fontWeight: "600",
-    color: Color.colorBlack,
-  },
-  usernameIcon: {
-    height: "13.91%",
-    width: "31.89%",
-    top: "36.44%",
-    right: "38.06%",
-    bottom: "68.75%",
-    left: "12.06%",
-  },
-  passwordIcon: {
-    height: "13.91%",
-    width: "35.89%",
-    top: "39.44%",
-    right: "38.06%",
-    bottom: "68.75%",
-    left: "12.06%",
-  },
-  accountSettingsOption2Inner: {
-    top: "57.34%",
-    bottom: "42.5%",
-    opacity: 30,
-  },
-  accountSettingsOption2Child1: {
-    top: "74.84%",
-    bottom: "25%",
-  },
-  rectanglePressable: {
-    height: "6.72%",
-    width: "59.17%",
-    top: "82.97%",
-    right: "20.28%",
-    bottom: "10.31%",
-    left: "20.56%",
-    borderRadius: 15,
-    backgroundColor: "#132A17",
-    position: "absolute",
-  },
-  editUserInfo: {
-    top: "84.23%",
-    left: "28.22%",
-    fontSize: 16,
-    color: "white",
-  },
-  accountSettingsOption2: {
+  Container1: {
     backgroundColor: Color.colorMediumseagreen,
     flex: 1,
     width: "100%",
     height: 640,
     overflow: "hidden",
+  },
+  BackButton: {
+    height: "15.47%",
+    width: "25.53%",
+    marginTop: "25",
+    marginLeft: "20",
+    position: "absolute",
+    overflow: "hidden",
+  },
+  Container2: {
+    backgroundColor: "#F9E2D0",
+    width: "82%", // Adjust width as needed
+    height: "65%", // Adjust height as needed
+    borderRadius: 15, // Optional: for rounded corners
+    // Center content vertically
+    alignItems: "center", // Center content horizontally
+    marginHorizontal: "45",
+    marginTop: "70",
+    flexDirection: "column",
+  },
+  Icon: {
+    marginTop: "10",
+  },
+  Child: {
+    backgroundColor: "white",
+    width: "70%",
+    height: "15%",
+    flexDirection: "row",
+    marginTop: "20",
+    alignItems: "center", // Center items vertically
+    justifyContent: "",
+    borderRadius: 50,
+  },
+  IconChild: {
+    marginLeft: "20",
+    marginBottom: "5"
+  },
+  ChildText: {
+    Color: "#132A17",
+    fontWeight: "bold",
+    marginHorizontal: "10",
+    marginVertical: "20",
+    fontSize: 20,
+  },
+  Container3: {
+    width: "80%", // Adjust width as needed
+    height: "10%", // Adjust height as needed
+    borderRadius: 15, // Optional: for rounded corners
+    alignItems: "center", // Center content horizontally
+    marginHorizontal: "45",
+    marginTop: "10",
+    flexDirection: "row",
+  },
+
+  EditButton: {
+    height: "70%",
+    width: "50%",
+    borderRadius: 15,
+    backgroundColor: "#132A17",
+    marginRight: "10",
+    marginLeft: "0",
+    elevation: 5
+  },
+  EditButton2: {
+    height: "70%",
+    width: "50%",
+    borderRadius: 15,
+    backgroundColor: "#FF0000",
+    marginRight: "10",
+    marginLeft: "0",
+    elevation: 5,
+    
+    
+  },
+  EditButtonText: {
+    textAlign: "center",
+    marginTop: "14",
+    color: "white",
+    fontWeight: "bold",
   },
 });
 
--- screens/UserInfo.js
+++ screens/UserInfo.js
@@ -16,209 +16,70 @@ const UserInfo = () => {
 
   // State to hold user input values
   const [username, setUsername] = React.useState("");
+  const [email, setEmail] = React.useState("");
   const [password, setPassword] = React.useState("");
 
   return (
     <View style={styles.userInfo}>
-      <Pressable style={[styles.arrowleft]} onPress={() => navigation.goBack()}>
+      <Pressable
+        style={[styles.arrowleft]}
+        onPress={() => navigation.navigate("AccountSettingsOption")}
+      >
         <Icon name="arrow-back" size={25} color="#132A17" />
       </Pressable>
-      <View style={styles.userInfoChild} />
-      <Text style={[styles.editUserInfoItem, styles.editUserInfoTypo]}>
-        EDIT USER INFO
-      </Text>
-      <View style={[styles.userInfoItem, styles.userShadowBox]} />
-      <View style={[styles.rectangleView, styles.userShadowBox]} />
-      <Text style={[styles.username, styles.emailTypo]}>USERNAME</Text>
 
-      <Text style={[styles.password, styles.emailTypo]}>PASSWORD</Text>
-      <View style={styles.userInfoChild1} />
-      <TextInput
-        style={[styles.userInfoItem, styles.userShadowBox]}
+      <View style={styles.container1}>
+        <Text style={styles.heading}> EDIT USER INFO </Text>
+      </View>
+      <View style={styles.container2}>
+      <Text style={styles.label}>
+          USERNAME
+        </Text>
+        <View style={styles.textBox}>
+        <TextInput
         placeholder="Enter Username"
         value={username}
         onChangeText={setUsername}
       />
+        </View>
+        <Text style={styles.label}>
+          EMAIL
+        </Text>
+
+        <View style={styles.textBox}>
+        
+        <TextInput
+        placeholder="Enter Email"
+        value={email}
+        onChangeText={setEmail}
+      />
+        </View>
+        <Text style={styles.label}>
+          PASSWORD
+        </Text>
+        <View style={styles.textBox}>
       <TextInput
-        style={[styles.rectangleView, styles.userShadowBox]}
         placeholder="Enter Password"
         secureTextEntry={true}
         value={password}
         onChangeText={setPassword}
       />
+      </View>
 
-      <View style={styles.userInfoChild1} />
       <Pressable
-        style={styles.saveChanges}
+        style={styles.saveChangesButton}
         onPress={() => navigation.navigate("AccountSettingsOption")}
       >
-        <Text style={[styles.saveChanges1, styles.editUserInfo]}>
+        <Text style={styles.saveChangesText}>
           SAVE CHANGES
         </Text>
       </Pressable>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  editUserInfoTypo: {
-    textAlign: "center",
-    fontSize: 24,
-    fontFamily: FontFamily.poppinsExtraBold,
-    fontWeight: "800",
-    color: Color.colorDarkslategray,
-    position: "absolute",
-  },
-  userShadowBox: {
-    borderWidth: 1,
-    borderColor: Color.colorSeagreen,
-    borderStyle: "solid",
-    left: "13.06%",
-    right: "9.17%",
-    width: "77.78%",
-    borderRadius: Border.br_xl,
-    shadowOpacity: 8,
-    elevation: 4,
-    shadowRadius: 6,
-    shadowOffset: {
-      width: 4,
-      height: 2,
-    },
-    shadowColor: "rgba(0, 0, 0, 0.25)",
-    height: "6.25%",
-    position: "absolute",
-    paddingHorizontal: 10,
-  },
-  emailTypo: {
-    color: "white",
-    fontSize: 18,
-    textAlign: "center",
-    fontFamily: FontFamily.poppinsExtraBold,
-    fontWeight: "800",
-    position: "absolute",
-    textShadowColor: "rgba(0, 0, 0, 0.75)",
-    textShadowOffset: { width: -1, height: 1 },
-    textShadowRadius: 2,
-  },
-  arrowleft: {
-    height: "15.47%",
-    width: "25.53%",
-    top: "7.28%",
-    right: "86.81%",
-    bottom: "93.25%",
-    left: "3.67%",
-    position: "absolute",
-    overflow: "hidden",
-  },
-  userInfoChild: {
-    width: "88.33%",
-    top: "18.19%",
-    right: "5.83%",
-    bottom: "81.56%",
-    left: "5.83%",
-    position: "absolute",
-    backgroundColor: Color.colorSeagreen,
-    borderRadius: Border.br_xl,
-    shadowOpacity: 1,
-    elevation: 4,
-    shadowRadius: 4,
-    shadowColor: "rgba(0, 0, 0, 0.25)",
-    shadowOffset: {
-      width: 0,
-      height: 4,
-    },
-    height: "6.25%",
-    position: "absolute",
-  },
-  editUserInfo: {
-    width: "75.83%",
-    top: "4.54%",
-    textAlign: "center",
-    fontFamily: FontFamily.poppinsSemiBold,
-    fontWeight: "600",
-    fontSize: 20,
-    height: "65.53%",
-    color: "#fffafa",
-  },
-  editUserInfoItem: {
-    width: "75.83%",
-    top: "18.54%",
-    textAlign: "center",
-    fontFamily: FontFamily.poppinsSemiBold,
-    fontWeight: "600",
-    fontSize: 20,
-    height: "65.53%",
-    left: 40,
-    textShadowColor: "rgba(0, 0, 0, 0.75)",
-    textShadowOffset: { width: -1, height: 1 },
-    textShadowRadius: 2,
-    letterSpacing: 1.5,
-  },
-  userInfoItem: {
-    top: "34.84%",
-    bottom: "63.91%",
-    fontSize: FontSize.size_mini,
-    height: "3.59%",
-  },
-  rectangleView: {
-    top: "61.5%",
-    bottom: "36.25%",
-    fontSize: FontSize.size_mini,
-    height: "3.59%",
-  },
-  username: {
-    top: "30.4%",
-    left: "6.11%",
-    fontSize: FontSize.size_mini,
-    width: "31.39%",
-    height: "3.59%",
-    color: "white",
-  },
-  email: {
-    top: "42.88%",
-    left: "0%",
-    fontSize: FontSize.size_mini,
-    width: "31.39%",
-    height: "3.59%",
-  },
-  password: {
-    top: "55.88%",
-    left: "6.11%",
-    fontSize: FontSize.size_mini,
-    width: "31.39%",
-    height: "3.59%",
-  },
-  userInfoChild1: {
-    height: "6.72%",
-    width: "59.17%",
-    top: "81.88%",
-    right: "18.06%",
-    bottom: "11.41%",
-    left: "22.78%",
-    borderRadius: 15,
-    backgroundColor: "#132A17",
-    position: "absolute",
-  },
-  saveChanges1: {
-    width: "45.83%",
-    top: "54.84%",
-    textAlign: "center",
-    fontFamily: FontFamily.poppinsSemiBold,
-    fontWeight: "600",
-    fontSize: 22,
-    height: "35.53%",
-  },
-  saveChanges: {
-    width: "59.17%",
-    height: "6.72%",
-    top: "81.88%",
-    left: "22.78%",
-    backgroundColor: Color.colorDarkslategray,
-    borderRadius: Border.br_mini,
-    justifyContent: "center",
-    alignItems: "center",
-    position: "absolute",
-  },
   userInfo: {
     backgroundColor: Color.colorMediumseagreen,
     flex: 1,
@@ -226,6 +87,71 @@ const styles = StyleSheet.create({
     height: 640,
     overflow: "hidden",
   },
+  arrowleft: {
+    height: "15.47%",
+    width: "25.53%",
+    marginTop: "25",
+    marginLeft: "20",
+    position: "absolute",
+    overflow: "hidden",
+  },
+  container1:{
+    backgroundColor: "#3A7D44",
+    width: "82%", // Adjust width as needed
+    height: "8%", // Adjust height as needed
+    borderRadius: 50, // Optional: for rounded corners
+    alignItems: "center", // Center content horizontally
+    marginHorizontal: "45",
+    marginTop: "90",
+    flexDirection: "column",
+  },
+  heading: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: "#132A17",
+    marginTop: '13'
+  },
+  container2:{
+    backgroundColor: "#3A7D44",
+    width: "82%", // Adjust width as needed
+    height: "50%", // Adjust height as needed
+    borderRadius: 25, // Optional: for rounded corners
+    alignItems: "stretch", // Center content horizontally
+    marginHorizontal: "45",
+    marginTop: "50",
+    flexDirection: "column",
+  },
+  textBox: {
+    height: "10%",
+    width: "78%",
+    backgroundColor: "#F6D4BA",
+    marginTop: 1,
+    marginLeft: "10%",
+    borderRadius: 10,
+  },
+  label: {
+    marginLeft: "10%",
+    marginTop: "9%",
+    fontWeight: "bold",
+    color: "white"
+  },
+  saveChangesButton: {
+    backgroundColor: "#132A17",
+    width: "40%",
+    height: "10%",
+    marginHorizontal: "30%",
+    marginTop: "10%",
+    borderRadius: 10,
+    textAlign: "center",
+    elevation: 10,
+  },
+  saveChangesText: {
+    textAlign: "center",
+    marginTop: 10,
+    fontWeight: "bold",
+    color: "white"
+  },
+  
 });
 
 export default UserInfo;
